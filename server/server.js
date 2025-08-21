a64e78e966c7aa88b5b56e8bd468140ad9131e66
Dambarudhara Rout <dambarudhararout1622@gmail.com>
2025-08-21 18:09:47 +0530

    Add getUsersForAdded GetUserSidebar and unseen message count

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 //create express app and HTTP server
 const app = express();
@@ -12,11 +13,13 @@ const server = http.createServer(app);
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+// Route setup
 app.use("/api/status", (req, res)=> res.send("Server is live"));
-
+app.use("/api/auth", userRouter);
 
 //connect to MongoDB
 await connectDB();
+
 //start server
 const PORT= process.env.PORT || 5000;
 server.listen(PORT,()=> console.log("Server is running on port: ", 
